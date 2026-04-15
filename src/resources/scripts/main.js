0ae1792f6f9666e3aa0bdd9ba111349bdb329025
elesey3510 <elesey3510@gmail.com>
2026-04-15 10:09:12 +0300

    bebebebe

--- src/resources/scripts/main.js
+++ src/resources/scripts/main.js
@@ -1,12 +1,12 @@
 // it is really useful for development
-var mod = Vars.mods.getMod("scheme-size")
-var get = (pkg) => mod.loader.loadClass(pkg).newInstance()
-
-// mod.loader is null on mobile devices
-if (Vars.mobile) get = (pkg) => null;
-
-const SchemeMain = mod.main
-const SchemeVars = get("scheme.SchemeVars")
-const SchemeUpdater = get("scheme.SchemeUpdater")
-const ServerIntegration = get("scheme.ServerIntegration")
-const ModedSchematics = get("scheme.moded.ModedSchematics")
+//var mod = Vars.mods.getMod("scheme-size")
+//var get = (pkg) => mod.loader.loadClass(pkg).newInstance()
+//
+//// mod.loader is null on mobile devices
+//if (Vars.mobile) get = (pkg) => null;
+//
+//const SchemeMain = mod.main
+//const SchemeVars = get("scheme.SchemeVars")
+//const SchemeUpdater = get("scheme.SchemeUpdater")
+//const ServerIntegration = get("scheme.ServerIntegration")
+//const ModedSchematics = get("scheme.moded.ModedSchematics")
